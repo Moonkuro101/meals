a69506489b1a762415d81163c58e978f7cd2eee7
Moonkuro101 <songpontonku@gmail.com>
2024-07-30 14:43:08 +0700

    add Gridview and Inkwell for tap the botton

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/categories.dart';
+import 'package:meals/screen/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -9,8 +9,7 @@ final theme = ThemeData(
     brightness: Brightness.dark,
     seedColor: const Color.fromARGB(255, 131, 57, 0),
   ),
-  textTheme: GoogleFonts.latoTextTheme(),
-);
+  textTheme: GoogleFonts.latoTextTheme(),);
 
 void main() {
   runApp(const App());
